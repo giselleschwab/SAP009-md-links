50116139de460bef4a72bcde2f12b195bc2e8d75
Giselle Schwab Silva <giselleschwab@gmail.com>
2023-04-26 18:41:10 -0300

    processando diretórios

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ function extraiLinks(texto) {
   const resultados = capturas.map(captura => ({[captura[1]]: [captura[2]]}));
   //usamos o método map para percorrer um array e retornar outro array com o resultado que queremos
   //utilizamos os indíces 1 e 2 que representam os dois grupos de Regex que montamos anteriormente
-  console.log(resultados);
+  return resultados.length !== 0 ? resultados : 'não há links no arquivo'
+  //verificamos o retorno para que ele não seja vazio e caso não haja um link ele retorna um erro
+  //com isso, se resultados.length for diferente de 0, recebemos a mensagem 'não há links no arquivo'
 }
 
 
@@ -22,9 +24,12 @@ function trataErro(erro) {
 //transformamos o código em assíncrono pois não sabemos o tamanho do arquivo
 function pegaArquivo(caminhoDoArquivo){
   const encoding = 'utf-8';
-  fs.promises.readFile(caminhoDoArquivo, encoding) 
+  return fs.promises.readFile(caminhoDoArquivo, encoding) 
     .then((texto) => extraiLinks(texto))
     .catch(trataErro)
 }
+//o método fs.promises.readFile é um método que garante que o JS não tenha problemas de sincronicidade ao processar arquivos grandes
 
 pegaArquivo('./arquivo/texto.md')
+
+export default pegaArquivo;
